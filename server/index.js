3912f51f4bc88b25dea162d8522e98aa4d64eaa4
activiteOCR <julien.terrier3@wanadoo.fr>
2024-01-07 19:42:31 +0100

    issue about public key recovering

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
-//const secp = require('@noble/secp256k1');
+const {secp256k1} =require("ethereum-cryptography/secp256k1");
+const {toHex, utf8ToBytes} =require("ethereum-cryptography/utils")
+
 const app = express();
 const cors = require("cors");
 const port = 3042;
@@ -23,10 +25,33 @@ app.post("/send", (req, res) => {
   // Get a signature from client-side application
   // Recover the public address from the signature
   
-  const { sender, signature, hash, recipient, amount } = req.body;
-  //console.log(signature, hash);
-  //const pubKey = secp.recoverPublicKey(hash, signature, signature.recovery);
-  //console.log(pubKey);
+  const { sender, signature, hashedValue, recipient, amount } = req.body;
+
+  // Convert object to JSON
+  const jsonString = JSON.stringify(hashedValue);
+
+  // Convert JSON string to hex
+  const hexString = Buffer.from(jsonString, 'utf-8').toString('hex');
+
+  //console.log('Object as Hex:', hexString);
+
+  const signatureRecovered = new secp256k1.Signature(
+    BigInt(signature.r),
+    BigInt(signature.s)
+  ).addRecoveryBit(signature.recovery)
+  
+  const pubKey = signatureRecovered.recoverPublicKey(hexString);
+  
+  // Compress the public key
+  const publicKey = pubKey.toHex(true);
+
+  //const tempAddr = publicKey.slice(1, 65);
+  //const hash = keccak256(tempAddr);
+  //const address = toHex(hash.slice(12, 32));
+
+  //const address = toHex(keccak256(hexToBytes(publicKey.slice(2))).slice(-20));
+  //console.log("address:" + address);
+
   setInitialBalance(sender);
   setInitialBalance(recipient);
 
